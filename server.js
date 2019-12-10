06c68c1276f674ac55164742ce44119bf87042bb
Aleksandr <duxard@gmail.com>
2019-12-10 01:02:03 -0500

    add JWT authorization

--- server.js
+++ server.js
@@ -13,10 +13,44 @@ app.get('/api', (req, res) => {
   });
 });
 
-app.post('/apia/posts', (req, res) => {
+app.post('/api/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretKey', (err, authData) => {
+    if(err) {
+      res.status(403).send({error: err.message});
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
+  });
+});
 
+app.post('/api/login', (req, res) => {
+  const user = {
+    id: 1,
+    username: 'Brad',
+    email: 'brad@gamil.com'
+  };
+
+  jwt.sign({ user }, 'secretKey', { expiresIn: '30s' }, (err, token) => {
+    res.json({ token });
+  });
 });
 
 app.listen(port, host, () => {
   console.log(`Server is running at ${host}:${port}`);
 });
+
+// Format of token
+// Authorization: Bearer <access_token>
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if(typeof bearerHeader !== 'undefined') {
+    req.token = bearerHeader.split(' ')[2];
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
